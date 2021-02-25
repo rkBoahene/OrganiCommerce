6e40aab2af22f01bfb9fcfcb6ad20e3d345f9905
rkboahene <richmond.boahene@akaditi.com>
2021-02-25 01:29:09 +0000

    added products, change dynamic category and product links

--- OganiCommerce/store/urls.py
+++ OganiCommerce/store/urls.py
@@ -4,5 +4,8 @@ from . import views
 app_name = 'store'
 
 urlpatterns = [
-    path('', views.product_list, name='product_list')
+    path('', views.product_list, name='product_list'),
+    path('<int:id>/<slug:slug>/', views.product_detail, name='product_detail'),
+    path('search/<slug:category_slug>/',
+         views.category_list, name='category_list')
 ]
--- OganiCommerce/store/views.py
+++ OganiCommerce/store/views.py
@@ -1,8 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Category, Product
 # Create your views here.
 
 
 def product_list(request):
     categories = Category.objects.all()
-    return render(request, 'store/index.html', {'categories': categories})
+    products = Product.objects.all()
+    return render(request, 'store/index.html', {'categories': categories, 'products': products})
+
+
+def product_detail(request, id, slug):
+    product = get_object_or_404(Product, id=id, slug=slug, available=True)
+    return render(request, 'store/product/detail.html', {'product': product})
+
+
+def category_list(request, category_slug):
+    categories = get_object_or_404(Category, slug=category_slug)
+    products = Product.objects.filter(category=categories)
+    return render(request, 'store/product/category.html', {'categories': categories, 'products': products})
